cee26266a0e54d62fce37748923dbefef20ade8d
Michael Stapelberg <michael@stapelberg.de>
2020-04-07 08:53:57 +0200

    mbr: include partuuid in disk signature
    related to https://github.com/gokrazy/gokrazy/issues/52

--- mbr/mbr.go
+++ mbr/mbr.go
@@ -15,15 +15,21 @@ type bootloaderParams struct {
 	CmdlineLBA uint32 // e.g. 8218
 }
 
-func Configure(vmlinuzLba, cmdlineLba uint32) [446]byte {
+func Configure(vmlinuzLba, cmdlineLba uint32, partuuid uint32) [446]byte {
 	buf := bytes.NewBuffer(make([]byte, 0, 446))
 	// buf.Write never fails
-	buf.Write(mbr[:433])
+	buf.Write(mbr[:432])
 	params := bootloaderParams{
 		CurrentLBA: vmlinuzLba,
 		CmdlineLBA: cmdlineLba,
 	}
 	binary.Write(buf, binary.LittleEndian, &params)
+	if pad := 440 - buf.Len(); pad > 0 {
+		buf.Write(bytes.Repeat([]byte{0}, pad))
+	}
+	// disk signature (for PARTUUID= root device Linux kernel parameter)
+	binary.Write(buf, binary.LittleEndian, partuuid)
+	binary.Write(buf, binary.LittleEndian, uint16(0x0000))
 	var b [446]byte
 	copy(b[:], buf.Bytes())
 	return b
