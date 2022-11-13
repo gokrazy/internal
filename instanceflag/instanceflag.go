6c14a9bfe678336e89d8c0f8930d41945d6fc57f
Michael Stapelberg <michael@stapelberg.de>
2022-11-13 17:45:24 +0100

    add instanceflag package
    related to https://github.com/gokrazy/gokrazy/issues/147

---
+++ instanceflag/instanceflag.go
@@ -0,0 +1,48 @@
+package instanceflag
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/pflag"
+)
+
+var (
+	instance  string
+	parentDir string
+)
+
+func RegisterPflags(fs *pflag.FlagSet) {
+	def := os.Getenv("GOKRAZY_INSTANCE")
+	if def == "" {
+		def = "gokrazy"
+	}
+	fs.StringVarP(&instance,
+		"instance",
+		"i",
+		def,
+		`instance, identified by hostname`)
+
+	def = os.Getenv("GOKRAZY_PARENT_DIR")
+	if def == "" {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			homeDir = fmt.Sprintf("os.UserHomeDir failed: %v", err)
+		}
+		def = filepath.Join(homeDir, "gokrazy")
+	}
+	fs.StringVar(&parentDir,
+		"parent_dir",
+		def,
+		`parent directory: contains one subdirectory per instance`)
+
+}
+
+func Instance() string {
+	return instance
+}
+
+func ParentDir() string {
+	return parentDir
+}
