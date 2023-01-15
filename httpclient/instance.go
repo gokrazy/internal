79b761a8d0402bc784801071033acba4e52c4ca6
Michael Stapelberg <michael@stapelberg.de>
2023-01-15 16:37:09 +0100

    httpclient: add For() function working with a *config.Struct

--- httpclient/instance.go
+++ httpclient/instance.go
@@ -1,13 +1,50 @@
 package httpclient
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 	"os"
 
 	"github.com/gokrazy/internal/config"
+	"github.com/gokrazy/internal/tlsflag"
 	"github.com/gokrazy/internal/updateflag"
 )
+
+func For(cfg *config.Struct) (_ *http.Client, foundMatchingCertificate bool, updateBaseURL *url.URL, _ error) {
+	schema := "http"
+	certPath, _, err := tlsflag.CertificatePathsFor(cfg.Hostname)
+	if err != nil {
+		return nil, false, nil, err
+	}
+	if certPath != "" {
+		schema = "https"
+	}
+
+	update, err := cfg.Update.WithFallbackToHostSpecific(cfg.Hostname)
+	if err != nil {
+		return nil, false, nil, err
+	}
+
+	if update.HTTPPort == "" {
+		update.HTTPPort = "80"
+	}
+
+	if update.HTTPSPort == "" {
+		update.HTTPSPort = "443"
+	}
+
+	updateBaseURL, err = updateflag.BaseURL(update.HTTPPort, schema, update.Hostname, update.HTTPPassword)
+	if err != nil {
+		return nil, false, nil, err
+	}
+
+	hc, fmc, err := GetTLSHttpClientByTLSFlag(tlsflag.GetUseTLS(), tlsflag.GetInsecure(), updateBaseURL)
+	if err != nil {
+		return nil, false, nil, fmt.Errorf("getting http client by tls flag: %v", err)
+	}
+	return hc, fmc, updateBaseURL, nil
+}
 
 func GetHTTPClientForInstance(instance string) (*http.Client, *url.URL, error) {
 	_, updateHostname := updateflag.GetUpdateTarget(instance)
