c51f6b8f941235f317f8c8c8f4b4a60328f8120d
Michael Stapelberg <michael@stapelberg.de>
2023-01-15 16:38:56 +0100

    httpclient: remove now-unused GetHTTPClientForInstance

--- httpclient/instance.go
+++ httpclient/instance.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
-	"os"
 
 	"github.com/gokrazy/internal/config"
 	"github.com/gokrazy/internal/tlsflag"
@@ -44,51 +43,4 @@ func For(cfg *config.Struct) (_ *http.Client, foundMatchingCertificate bool, upd
 		return nil, false, nil, fmt.Errorf("getting http client by tls flag: %v", err)
 	}
 	return hc, fmc, updateBaseURL, nil
-}
-
-func GetHTTPClientForInstance(instance string) (*http.Client, *url.URL, error) {
-	_, updateHostname := updateflag.GetUpdateTarget(instance)
-	pw, err := config.HostnameSpecific(updateHostname).ReadFile("http-password.txt")
-	if err != nil {
-		return nil, nil, err
-	}
-
-	port, err := config.HostnameSpecific(updateHostname).ReadFile("http-port.txt")
-	if err != nil && !os.IsNotExist(err) {
-		return nil, nil, err
-
-	}
-
-	cert, err := config.HostnameSpecific(updateHostname).ReadFile("cert.pem")
-	if err != nil && !os.IsNotExist(err) {
-		return nil, nil, err
-
-	}
-
-	tlsFlag := ""
-	scheme := "http"
-	if cert != "" {
-		tlsFlag = "self-signed"
-		scheme = "https"
-	}
-
-	if port == "" {
-		if scheme == "https" {
-			port = "443"
-		} else {
-			port = "80"
-		}
-	}
-
-	updateBaseUrl, err := updateflag.BaseURL(port, scheme, updateHostname, pw)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	httpClient, _, err := GetTLSHttpClientByTLSFlag(tlsFlag, false, updateBaseUrl)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return httpClient, updateBaseUrl, err
 }
