95a29c8f05932244570635d493c80f8a3a32dce9
Michael Stapelberg <michael@stapelberg.de>
2025-12-05 17:43:05 +0100

    updateflag: introduce Value type to avoid global state
    related to https://github.com/gokrazy/tools/pull/68

--- updateflag/updateflag.go
+++ updateflag/updateflag.go
@@ -7,50 +7,6 @@ import (
 
 var update string
 
-func GetUpdateTarget(hostname string) (defaultPassword, updateHostname string) {
-	if update == "" {
-		// -update not set
-		return "", hostname
-	}
-	if update == "yes" {
-		// -update=yes
-		return "", hostname
-	}
-	if strings.HasPrefix(update, ":") {
-		// port number syntax, e.g. -update=:2080
-		return "", hostname
-	}
-	// -update=<url> syntax
-	u, err := url.Parse(update)
-	if err != nil {
-		return "", hostname
-	}
-	defaultPassword, _ = u.User.Password()
-	return defaultPassword, u.Host
-}
-
-func BaseURL(httpPort, httpsPort, schema, hostname, pw string) (*url.URL, error) {
-	if update != "yes" && !strings.HasPrefix(update, ":") {
-		// already fully qualified, nothing to add
-		return url.Parse(update)
-	}
-	port := httpPort
-	defaultPort := "80"
-	if schema == "https" {
-		port = httpsPort
-		defaultPort = "443"
-	}
-	if strings.HasPrefix(update, ":") {
-		port = strings.TrimPrefix(update, ":")
-	}
-	update = schema + "://gokrazy:" + pw + "@" + hostname
-	if port != defaultPort {
-		update += ":" + port
-	}
-	update += "/"
-	return url.Parse(update)
-}
-
 func NewInstallation() bool {
 	return update == ""
 }
@@ -58,3 +14,61 @@ func NewInstallation() bool {
 func SetUpdate(u string) { update = u }
 
 func GetUpdate() string { return update }
+
+// Deprecated: use [Value.GetUpdateTarget]
+func GetUpdateTarget(hostname string) (defaultPassword, updateHostname string) {
+	return Value{update}.GetUpdateTarget(hostname)
+}
+
+// Deprecated: use [Value.BaseURL]
+func BaseURL(httpPort, httpsPort, schema, hostname, pw string) (*url.URL, error) {
+	return Value{update}.BaseURL(httpPort, httpsPort, schema, hostname, pw)
+}
+
+type Value struct {
+	Update string
+}
+
+func (v Value) GetUpdateTarget(hostname string) (defaultPassword, updateHostname string) {
+	if v.Update == "" {
+		// -update not set
+		return "", hostname
+	}
+	if v.Update == "yes" {
+		// -update=yes
+		return "", hostname
+	}
+	if strings.HasPrefix(v.Update, ":") {
+		// port number syntax, e.g. -update=:2080
+		return "", hostname
+	}
+	// -update=<url> syntax
+	u, err := url.Parse(v.Update)
+	if err != nil {
+		return "", hostname
+	}
+	defaultPassword, _ = u.User.Password()
+	return defaultPassword, u.Host
+}
+
+func (v Value) BaseURL(httpPort, httpsPort, schema, hostname, pw string) (*url.URL, error) {
+	if v.Update != "yes" && !strings.HasPrefix(v.Update, ":") {
+		// already fully qualified, nothing to add
+		return url.Parse(v.Update)
+	}
+	port := httpPort
+	defaultPort := "80"
+	if schema == "https" {
+		port = httpsPort
+		defaultPort = "443"
+	}
+	if strings.HasPrefix(v.Update, ":") {
+		port = strings.TrimPrefix(v.Update, ":")
+	}
+	v.Update = schema + "://gokrazy:" + pw + "@" + hostname
+	if port != defaultPort {
+		v.Update += ":" + port
+	}
+	v.Update += "/"
+	return url.Parse(v.Update)
+}
